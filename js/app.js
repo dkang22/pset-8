6ed7580ab6563877d1b30fabfe6e9d4c227523a9
Danielle Kang <daniellekang24@gmail.com>
2020-01-31 10:22:09 -0500

    halfway through walkthrough

---
+++ js/app.js
@@ -0,0 +1,42 @@
+///////////////////// CONSTANTS /////////////////////////////////////
+
+
+///////////////////// APP STATE (VARIABLES) /////////////////////////
+let board;
+let turn;
+
+///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+const squares = Array.from(document.querySelectorAll("#board div"));
+
+///////////////////// EVENT LISTENERS ///////////////////////////////
+window.onload = init;
+document.getElementById("board").onclick = takeTurn;
+
+///////////////////// FUNCTIONS /////////////////////////////////////
+function init() {
+  board = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+  ];
+
+  turn = "X";
+  render();
+}
+
+function render() {
+  board.forEach(function(mark, index) {
+    squares[index].textContent = mark;    // writes an X or an O on board
+  });
+}
+
+function takeTurn(e) {
+  let index = squares.findIndex(function(square) {
+    return square === e.target;
+  });
+
+  board[index] = turn;
+  turn = turn === "X" ? "O" : "X";
+
+  render();
+}
